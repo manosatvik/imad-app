fb06c2d2b377d80ea7afbbbafd4e1394efff56f8
manosatvik <manosatvik@gmail.com>
2018-02-19 22:53:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,11 +14,13 @@ app.get('/article-one',function(req,res){
 });
 
 app.get('/article-two',function(req,res){
-   res.send("you requested article two and it will be sent"); 
+
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html')); 
 });
 
 app.get('/article-three',function(req,res){
-   res.send("you requested article three and it will be sent"); 
+res.sendFile(path.join(__dirname, 'ui', 'article-three.html')); 
+    
 });
 
 app.get('/ui/style.css', function (req, res) {
