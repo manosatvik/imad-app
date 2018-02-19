799f4de808d9051e03093412eca0285e1623575a
manosatvik <manosatvik@gmail.com>
2018-02-19 22:47:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-   res.send("you requested article one and it will be sent"); 
+   res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
 });
 
 app.get('/article-two',function(req,res){
