3c51582d84571015e58dbd334c783c43d9d36bf0
manosatvik <manosatvik@gmail.com>
2018-02-20 17:47:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById("main-text").innerHTML="Hello text is chan
 //move image
 
 var image = document.getElementById("image");
-
+var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLeft + 10;
     image.style.marginLeft = marginLeft + "px";
