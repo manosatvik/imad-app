76a9531c474ad6154097e90ef45f750b46fb9038
manosatvik <manosatvik@gmail.com>
2018-02-20 17:41:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,11 @@
 console.log('Loaded!');
 
 var element = document.getElementById("main-text").innerHTML="Hello text is changed";
+
+//move image
+
+var image = document.getElementById("image");
+
+image.onclick = function(){
+    image.style.marginLeft="100px";
+}
