20d9593e180803de0640f1fa7e6a1bb5ba532c8b
manosatvik <manosatvik@gmail.com>
2018-02-22 12:08:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,21 +2,21 @@ var button = document.getElementById("counter");
 var counter = 0;
 button.onclick = function(){
     
-    var request =  new XMLHttpRequest();
-    
-    //capture the response and store it in counter variable
-    
-    request.onreadystatechange = function(){
-        if(request.readyState===XMLHttpRequest.DONE)
+    //make a new request
+    var request = new XMLHttpRequest();
+    //if request state changes
+    request.onreadystatechange = func(){
+        if(request.readystate === XMLHttpRequest.DONE)
         {
-            if(request.status===200)
+            if(request.status == 200)
             {
-                var counter = request.responseText;
-                var span = document.getElementById("count");
-    span.innerHTML=counter.toString();
+                var counter = request.responseText();
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
             }
         }
     };
     request.open('GET','http://manosatvik.imad.hasura-app.io/counter',true);
     request.send(null);
+    
 };
