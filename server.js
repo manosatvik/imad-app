0fc7c4c7df8968609f0e6676b7489c014daebecb
manosatvik <manosatvik@gmail.com>
2018-02-23 21:59:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,6 +85,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+names = [];
 app.get('/submit-name/:name',function(req,res){
     name = req.params.name;
     names=push(name);
