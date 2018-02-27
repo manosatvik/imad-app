c2bd804e406b318a56ab6b0bae3620a3091986ee
manosatvik <manosatvik@gmail.com>
2018-02-27 19:55:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var pool = require('pg').pool;
 var app = express();
 app.use(morgan('combined'));
+
+var config={
+    user:'manosatvik',
+    database:'manosatvik',
+    host:'db-imad.hasura-app.io'
+    port:'5432'
+    password:process.env.DB_PASSWORD
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -63,6 +71,19 @@ var counter = 0;
 app.get('/counter', function (req, res) {
   counter = counter + 1;
   res.send(counter.toString());
+});
+
+var pool = new pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('select * from usertable'),function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            res.send(JSON.stringify(result));
+        }
+    }
 });
 
 names = [];
