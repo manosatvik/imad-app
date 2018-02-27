cb30a35187eb0b0eb43f51049675cf1c1043784d
manosatvik <manosatvik@gmail.com>
2018-02-27 19:58:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ app.use(morgan('combined'));
 var config={
     user:'manosatvik',
     database:'manosatvik',
-    host:'db-imad.hasura-app.io'
-    port:'5432'
+    host:'db-imad.hasura-app.io',
+    port:'5432',
     password:process.env.DB_PASSWORD
-}
+};
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
